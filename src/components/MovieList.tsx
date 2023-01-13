1d617b13651815c29a35e16943b5a32531fdff09
KevLes <kevin.leskinen@knowit.se>
2023-01-13 15:49:46 +0100

    Added simple pagination

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,15 +7,19 @@ const MovieList = () => {
   const [movies, setMovies] = useState<Array<any>>();
   const [searchTerm, setSearchTerm] = useState("");
   const [typeFilter, setTypeFilter] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     if (searchTerm.length > 3) {
       fetchAllMovies(searchTerm);
     }
-  }, [searchTerm, typeFilter]);
+  }, [searchTerm, typeFilter, currentPage]);
+
+  console.log(currentPage);
+  
 
   const fetchAllMovies = async (searchTerm: string) => {
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=72e2b6a0&s=${searchTerm}&type=${typeFilter}`
+      `http://www.omdbapi.com/?apikey=72e2b6a0&s=${searchTerm}&type=${typeFilter}&page=${currentPage}`
     );
     const data = await response.json();
     setMovies(data.Search);
@@ -31,7 +35,10 @@ const MovieList = () => {
           placeholder="Search..."
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <select className={styles.selectFilter} onChange={(e) => setTypeFilter(e.target.value)}>
+        <select
+          className={styles.selectFilter}
+          onChange={(e) => setTypeFilter(e.target.value)}
+        >
           <option value=" ">Choose type</option>
           <option value=" ">All</option>
           <option value="movie">Movies</option>
@@ -52,12 +59,20 @@ const MovieList = () => {
                       height={443}
                     />
                   )}
-                  <h3><Link href={`/title/${movie.imdbID}`}>{movie.Title}</Link></h3>
+                  <h3>
+                    <Link href={`/title/${movie.imdbID}`}>{movie.Title}</Link>
+                  </h3>
                   <p>{movie.Year}</p>
                 </div>
               );
             })}
         </div>
+        {movies && (
+          <div>
+            {currentPage !== 1 && <button onClick={() => setCurrentPage(currentPage - 1)}>Back</button>}
+            {movies?.length === 10 && <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>}
+          </div>
+        )}
       </div>
     </div>
   );
